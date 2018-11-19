066b70197fabffc4de3c718e688ff1ead0eba2cb
ashish-chopra <sendtoashishchopra@gmail.com>
2018-11-19 11:25:08 -0800

    angular basic code committed

---
+++ src/scripts/app.js
@@ -0,0 +1,68 @@
+(function () {
+    angular
+        .module('BrowseAssist', [
+            'ui.router',
+            'treeControl'
+        ])
+        .config(configureApp)
+        .controller("RootController", rootController)
+        .controller('AssistController', assistController)
+        .controller('ThumbnailController', thumbnailController);
+
+
+    configureApp.$inject = ['$urlRouterProvider', '$stateProvider'];
+    function configureApp($urlRouterProvider, $stateProvider) {
+        console.log('configuration done');
+        $urlRouterProvider.otherwise("/");
+        $stateProvider
+            .state({
+                name: 'home',
+                url: '/',
+                templateUrl: 'templates/home.html',
+                controller: thumbnailController,
+                controllerAs: "thumbCtrl"
+
+            })
+            .state({
+                name: 'assist', 
+                url: "/assist",
+                templateUrl: "templates/assist.html"
+            });
+    }
+
+
+    rootController.$inject = ['$http'];
+    function rootController($http) {
+        console.log('running rootController');
+        var vm = this;
+        vm.treeOptions = {
+            nodeChildren: "children",
+            dirSelectable: true,
+            injectClasses: {
+                iExpanded: "fas fa-folder-open",
+                iCollapsed: "fas fa-folder",
+                iLeaf: "far fa-file"
+            }
+        };
+        $http.get('data/file.json').then(function(response) {
+            vm.treeModel = response.data;
+            console.log('data received', vm.treeModel);
+        });
+    }
+
+    assistController.$inject = [];
+    function assistController() {
+        var vm = this;
+    }
+
+
+    thumbnailController.$inject = ['$http'];
+    function thumbnailController($http) {
+        var vm = this;
+
+        $http.get('data/file.json').then(function(data) {
+            vm.data = data.data;
+        });
+    }
+
+}());
