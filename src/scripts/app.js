17b873925b139d17d8cf56a6b6753bd5fbd9f307
ashish-chopra <sendtoashishchopra@gmail.com>
2018-11-26 02:50:40 -0800

    added browse assist view code

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,12 +2,12 @@
     angular
         .module('BrowseAssist', [
             'ui.router',
-            'treeControl'
+            'treeControl',
+            'ui.bootstrap'
         ])
         .config(configureApp)
         .controller("RootController", rootController)
-        .controller('AssistController', assistController)
-        .controller('ThumbnailController', thumbnailController);
+        .controller('AssistController', assistController);
 
 
     configureApp.$inject = ['$urlRouterProvider', '$stateProvider'];
@@ -19,12 +19,9 @@
                 name: 'home',
                 url: '/:id',
                 templateUrl: 'templates/home.html',
-                controller: thumbnailController,
-                controllerAs: "thumbCtrl"
-
             })
             .state({
-                name: 'assist', 
+                name: 'assist',
                 url: "/assist/:id",
                 templateUrl: "templates/assist.html"
             });
@@ -42,20 +39,20 @@
             nodeChildren: "children",
             dirSelectable: true,
             injectClasses: {
-                iExpanded: "fas fa-folder-open",
-                iCollapsed: "fas fa-folder",
-                iLeaf: "far fa-file"
+                iExpanded: "fa fa-folder-open",
+                iCollapsed: "fa fa-folder",
+                iLeaf: "fa fa-file"
             }
         };
-        $http.get('data/file.json').then(function(response) {
+        $http.get('data/file.json').then(function (response) {
             if (response.data) {
                 vm.selectedNode = response.data;
-                response.data = { name: "", id: "#", children: [response.data]};
+                response.data = { name: "", id: "#", children: [response.data] };
             }
             vm.treeModel = response.data;
         });
 
-        vm.nodeSelected = function(node, selected) {
+        vm.nodeSelected = function (node, selected) {
             if (node.children) {
                 vm.selectedNode = node;
                 vm.rootId = node.id;
@@ -63,12 +60,12 @@
             }
         }
 
-        vm.changeView = function(stateName) {
+        vm.changeView = function (stateName) {
             vm.currentState = stateName;
             transition();
         }
 
-        vm.takeAction = function(node) {
+        vm.takeAction = function (node) {
             if (node.children) {
                 drillTo(node.id);
             } else {
@@ -76,26 +73,26 @@
             }
         }
 
-        vm.back = function() {
+        vm.back = function () {
             $window.history.back();
         }
-        drillTo = function(id) {
+        drillTo = function (id) {
             vm.selectedNode = getNodeBy(id);
             vm.rootId = vm.selectedNode.id;
             transition();
         }
 
-        
-        transition = function() {
-            $state.go(vm.currentState, {id: vm.rootId});
+
+        transition = function () {
+            $state.go(vm.currentState, { id: vm.rootId });
         }
 
-        getNodeBy = function(id) {
+        getNodeBy = function (id) {
             root = vm.treeModel;
             return getNode(root, id);
         }
-        
-        getNode = function(parent, id) {
+
+        getNode = function (parent, id) {
             let result = null;
             if (parent) {
                 if (parent.id == id) {
@@ -109,22 +106,82 @@
                             if (result) break;
                         }
                     }
-                    
+
                 }
             }
             return result;
         }
     }
 
-    assistController.$inject = [];
-    function assistController() {
+    assistController.$inject = ['$element', '$scope'];
+    function assistController($element, $scope) {
         var vm = this;
-    }
+        var parentCtrl = $scope.$parent.rCtrl;
+
+        let
+            radialPoint = (x, y) => {
+                return [(y = +y) * Math.cos(x -= Math.PI / 2), y * Math.sin(x)];
+            },
+
+            tree = d3.tree()
+                .size([2 * Math.PI, 180])
+                .separation((a, b) => (a.parent == b.parent ? 1 : 2) / a.depth),
+
+            root = tree(d3.hierarchy(parentCtrl.selectedNode));
 
 
-    thumbnailController.$inject = ['$http', '$stateParams'];
-    function thumbnailController($http, $stateParams) {
-       
+        var svg = d3.select("svg"),
+            width = $element[0].clientWidth,
+            height = $element[0].clientHeight;
+        const g = svg.append("g")
+            .attr("transform", `translate(${width / 2},${height / 2})`);
+        const chart = g.append("g");
+        var link = chart.selectAll(".link")
+            .data(root.links())
+            .enter().append("path")
+            .attr("class", "link")
+            .attr("d", d3.linkRadial()
+                .angle(function (d) { return d.x; })
+                .radius(function (d) { return d.y; }));
+
+        var node = chart.selectAll(".node")
+            .data(root.descendants())
+            .enter().append("g")
+            .attr("class", function (d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
+            .attr("transform", function (d) { return "translate(" + radialPoint(d.x, d.y) + ")"; })
+            .on("mouseover", function () {
+                d3.select(this).classed("active", true);
+            }).on("mouseout", function () {
+                d3.select(this).classed("active", false);
+            });
+
+        node.append("image")
+            .attr("x", -6)
+            .attr("y", -6)
+            .attr("width", 14)
+            .attr("height", 14)
+            .attr("transform", function (d) { return "rotate(" + (d.x < Math.PI ? d.x - Math.PI / 2 : d.x + Math.PI / 2) * 180 / Math.PI + ")"; })
+            .attr("xlink:href", function (d) { return d.children ? "img/favicon-folder.ico" : "img/favicon-file.ico" });
+
+        node.append("text")
+            .attr("dy", "0.31rem")
+            .attr("x", function (d) { return d.x < Math.PI === !d.children ? 10 : -10; })
+            .attr("text-anchor", function (d) { return d.x < Math.PI === !d.children ? "start" : "end"; })
+            .attr("transform", function (d) { return "rotate(" + (d.x < Math.PI ? d.x - Math.PI / 2 : d.x + Math.PI / 2) * 180 / Math.PI + ")"; })
+            .text(function (d) { return d.data.name; });
+
+        var zoom = d3.zoom()
+            .scaleExtent([1, 40])
+            .translateExtent([[-100, -100], [width + 90, height + 100]])
+            .on("zoom", zoomed);
+
+
+        svg.call(zoom)
+            .on("dblclick.zoom", null);
+        function zoomed() {
+            chart.attr("transform", d3.event.transform);
+        }
+
     }
 
 }());
